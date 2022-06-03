1e4b53ee0ef40a81e20527a3a7ac7040ac38ed08
Auxésio <gabrielbatista34368@gmail.com>
2022-06-03 14:42:31 -0300

    Resgate BD página Edição

--- costs/src/App.js
+++ costs/src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
 
 
 import Container from './components/layout/Container'
@@ -22,6 +23,7 @@ function App() {
           <Route path='/company' element={<Company />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='/newproject' element={<NewProject />}></Route>
+          <Route path='/project/:id' element={<Project />}></Route>
         </Routes>
       </Container>
       <Footer />
