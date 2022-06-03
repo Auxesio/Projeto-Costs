8f5cf048a1831cdfc029910e09f3a5bdd3d89b77
Auxésio <gabrielbatista34368@gmail.com>
2022-06-03 14:05:44 -0300

    Adição do Loader
    Animação de carregamento adicionado na página "Meus projetos"

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from "../layout/LinkButtom"
 import ProjectCard from "../project/ProjectCard"
 
@@ -11,6 +12,7 @@ import styles from './Projects.module.css'
 
 function Projects() {
     const [ projects, setProjects ] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -19,8 +21,8 @@ function Projects() {
     }
 
     useEffect(() => {
-
-        fetch('http://localhost:5000/projects', {
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
             method: 'GET',
             headers: {
                 'Contenty-Type':'application/json',
@@ -30,8 +32,10 @@ function Projects() {
         .then(data => {
             console.log(data)
             setProjects(data)
+            setRemoveLoading(true)
         })
         .catch(err => console.log(err))
+        }, 3000)
     }, [])
 
 
@@ -54,6 +58,12 @@ function Projects() {
                             key={project.id}
                         />
                     ))}
+                    {!removeLoading && <Loading />}
+                    {removeLoading && projects.length === 0 && (
+                        <p>Não há projetos cadastrados!</p>
+                    )
+
+                    }
             </Container>
         </div>
     )
